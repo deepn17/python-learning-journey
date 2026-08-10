25125171348575e26a8a023f9ecc9c45341ac5b0
Deepaks17 <deepujc@gmail.com>
2026-08-10 12:59:42 +0000

    Add BMI Calculation to profile card

--- day01/profile_card.py
+++ day01/profile_card.py
@@ -4,3 +4,6 @@ name = input("Enter your name: ")
 age = int(input("Enter your current age: "))
 city = input("Enter the name of the city you currently live in: ")
 
+weight_kg = float(input("Enter your weight in kg: "))
+height_m = float(input("Enter your height in meters: "))
+bmi = weight_kg / (height_m ** 2)

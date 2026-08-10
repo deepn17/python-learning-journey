fe4a48d6bd33195cde4cc9ae10694f945b8e43c1
Deepaks17 <deepujc@gmail.com>
2026-08-10 13:37:01 +0000

    Completed Mad Libs Generator

---
+++ day01/challenges.py
@@ -0,0 +1,12 @@
+"""
+1. 1. Mad Libs Generator — Ask for a noun, a verb, an adjective, and a place.
+Print a silly sentence using all four. Example: "The purple elephant danced gracefully at the supermarket."
+"""
+print(f"\n--- Mad Libs Generator")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjective: ")
+place = input("Enter a place: ")
+
+print(f"The {adjective} {noun} {verb} at the {place}")
+

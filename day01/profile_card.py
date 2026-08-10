53e1bc0fe54c9dd228bd3f20188748cb4797bc12
Deepaks17 <deepujc@gmail.com>
2026-08-10 12:56:18 +0000

    Add basic input collection for profile card

---
+++ day01/profile_card.py
@@ -0,0 +1,6 @@
+print("=== Personal Profile Card Generator ===")
+
+name = input("Enter your name: ")
+age = int(input("Enter your current age: "))
+city = input("Enter the name of the city you currently live in: ")
+

06334d8e1b5e9fa90165e111ff5058bea0170deb
deepn17 <deepujc@gmail.com>
2026-09-08 10:29:12 +0530

    set the range of guesses and maxnimum number for that range

--- day03/mini-projects/number_guesser.py
+++ day03/mini-projects/number_guesser.py
@@ -5,8 +5,8 @@ print("=" * 60)
 # difficulty level
 print("\n--- Choose Your Difficulty Level ---")
 print("1. Easy   (1 - 10, unlimited guesses)")
-print("2. Medium (1 - 50, 10 guesses")
-print("3. Hard   (1 - 100, 7 guesses")
+print("2. Medium (1 - 50, 10 guesses)")
+print("3. Hard   (1 - 100, 7 guesses)")
 
 level = input("Pick a level (1 - 3): ").strip()
 if level in ("1", "2", "3"):
@@ -15,3 +15,11 @@ else:
     print("Invalid choice. Please enter 1, 2, or 3.")
 
 # set the range and guess limit based on difficulty level
+if level == "1":
+    max_num, max_guesses = 10, 999
+elif level == "2":
+    max_num, max_guesses = 50, 10
+else:
+    max_num, max_guesses = 100, 7
+
+print(f"Maximum Number: {max_num}, Maximum Guesses:  {max_guesses}")
